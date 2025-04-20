0f4de3ed239b74f25f488ad654606f4f7a973ed3
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-04-20 09:30:01 +0800

    refactor: move navbar to root and login pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+import Navbar from '@/app/components/Navbar'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-lg">
-            <h1 className="text-5xl font-bold">Turn Studying into a Multiplayer Game!</h1>
-            <p className="py-6">
-              Your virtual study group—whenever, wherever.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+    <>
+      <Navbar />
+
+      <main>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content text-center">
+            <div className="max-w-lg">
+              <h1 className="text-5xl font-bold">Turn Studying into a Multiplayer Game!</h1>
+              <p className="py-6">
+                Your virtual study group—whenever, wherever.
+              </p>
+              <Link href='/login'>
+                <button className="btn btn-primary">Get Started</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
