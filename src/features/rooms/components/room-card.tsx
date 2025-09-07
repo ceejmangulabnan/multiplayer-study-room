d103d8a8be511e67054e7671104e0a1ca53ce0f9
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-09-07 14:31:02 +0800

    feat: room card shows icon for private rooms

--- src/features/rooms/components/room-card.tsx
+++ src/features/rooms/components/room-card.tsx
@@ -1,19 +1,30 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card"
 import Link from 'next/link'
+import { Card, CardContent, CardTitle } from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { ArrowRight, Lock } from 'lucide-react'
+import { Room } from '@/types/rooms-types'
 
-const RoomCard = ({ room }: { room: any }) => {
+const RoomCard = ({ room }: { room: Room }) => {
   return (
     <Card key={room.name} className="flex flex-col">
       <CardContent className="p-4 flex flex-col grow justify-between">
         <div className="grow">
-          <CardTitle className="text-lg hover:underline underline-offset-4">
-            <Link href="/dashboard">{room.name}</Link>
-          </CardTitle>
+          <div className="flex justify-between items-start">
+            <CardTitle className="text-lg hover:underline underline-offset-4">
+              <Link href="/dashboard">{room.name}</Link>
+            </CardTitle>
+            {
+              room.is_private && <Lock size={16} className='m-1 text-muted-foreground flex-shrink-0' />
+            }
+          </div>
           <p>{room.description}</p>
         </div>
-        <div className="flex justify-between text-xs text-muted-foreground mt-8">
-          <p>{room.members} members</p>
-          <p>{room.online} online</p>
+        <div className="flex justify-end text-xs text-muted-foreground mt-8">
+          <Button asChild>
+            <Link href={`/rooms/room/${room.short_id}`}>
+              View Room <ArrowRight />
+            </Link>
+          </Button>
         </div>
       </CardContent>
     </Card>
