a17365ea09a1f3080215ab9888c6ac7c97609fed
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2025-09-09 17:01:09 +0800

    style: align room cards

--- src/features/rooms/components/room-card.tsx
+++ src/features/rooms/components/room-card.tsx
@@ -6,7 +6,7 @@ import { Room } from '@/types/rooms-types'
 
 const RoomCard = ({ room }: { room: Room }) => {
   return (
-    <Card key={room.name} className="flex flex-col">
+    <Card key={room.name} className="h-full flex flex-col">
       <CardContent className="p-4 flex flex-col grow justify-between">
         <div className="grow">
           <div className="flex justify-between items-start">
